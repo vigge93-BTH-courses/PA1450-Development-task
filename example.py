9793273480f91f58ae370d1c3114b2bcdd2b0594
Victor <victarv@gmail.com>
2020-05-04 21:24:54 +0200

    Added examples

---
+++ example.py
@@ -0,0 +1,17 @@
+"""Example module."""
+
+
+def add(a, b):
+    """Add two numbers.
+
+    Add two numbers and return the result.
+
+    Args:
+        a: number
+        b: number
+    Returns:
+        The sum of a and b.
+    Raises:
+    TypeError: If the objects can't be added together.
+    """
+    return a + b
